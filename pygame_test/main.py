55f94a2da9a5f458c4681a35bf9649e87e3eefe4
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2022-03-12 13:38:08 +0300

    Add files via upload

--- pygame_test/main.py
+++ pygame_test/main.py
@@ -1,5 +1,5 @@
 import pygame
-frop player import Player
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -12,8 +12,18 @@ all_sprites.add(player)
 clock = pygame.time.Clock()
 
 while True: 
+	screen.fill((0, 0, 0))
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_DOWN:
+				player.rect.y += 10
+			if event.key == pygame.K_UP:
+				player.rect.y -= 10
 
 		all_sprites.update()
+		all_sprites.draw(screen)
+		pygame.display.flip()
+		clock.tick(60)
--- pygame_test/player.py
+++ pygame_test/player.py
@@ -3,3 +3,7 @@ import pygame
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
+		self.image = pygame.Surface((25, 150))
+		self.image.fill((255, 255, 255))
+		self.rect = self.image.get_rect()
+		self.rect.center = (25, 300)
