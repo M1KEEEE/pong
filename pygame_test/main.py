1fb831a17ec3dc579232870aba2035bc6683e516
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2022-03-16 17:21:31 +0300

    Add files via upload

--- pygame_test/main.py
+++ pygame_test/main.py
@@ -1,17 +1,17 @@
 import pygame
 from player import Player
 
+
+# настройки экрана
 WIDTH = 800
 HEIGHT = 600
-
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Моя игра")
 
-all_sprites = pygame.sprite.Group()
-player = Player(WIDTH, HEIGHT)
-all_sprites.add(player)
-
+# настройки игроков
+player = Player(screen, centerx=100)
+opponent = Player(screen, centerx=700, is_auto=True)
 clock = pygame.time.Clock()
 
 while True: 
@@ -20,13 +20,15 @@ while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_DOWN:
-				player.rect.y += 10
-			if event.key == pygame.K_UP:
-				player.rect.y -= 10
 
-		all_sprites.update()
-		all_sprites.draw(screen)
-		pygame.display.flip()
-		clock.tick(60)
+	keys = pygame.key.get_pressed()
+
+# вызов функций
+	player.draw()
+	player.control(keys)
+	opponent.draw()
+	opponent.control(keys)
+	pygame.display.flip()
+	clock.tick(60)
+
+pygame.quit()
